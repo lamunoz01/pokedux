626393e8d960850cb5a6963b054fe84c1f18e4b5
Laura B. Ahumada M. <lbahumada@open.com.co>
2024-06-12 10:27:02 -0500

    Peticiones asincronas

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,17 +1,23 @@
 import PokemonCard from "./PokemonCard";
 
 const PokemonList = ({ pokemons }) => {
-    return (
-      <div className='PokemonList'>
-        {pokemons.map((pokemon) => {
-          return <PokemonCard name={pokemon.name} key={pokemon.name} />;
-        })}
-      </div>
-    );
-  };
-  
-  PokemonList.defaultProps = {
-    pokemons: Array(10).fill(''), //Creara un arreglo de 10 posiciones donde cada posición se llena con el valor '' Es decir ['','','','','','','','','','']
-  };
-  
-  export default PokemonList;
+  return (
+    <div className="PokemonList">
+      {pokemons.map((pokemon) => {
+        return (
+          <PokemonCard
+            name={pokemon.name}
+            key={pokemon.name}
+            image={pokemon.sprites.front_default}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+PokemonList.defaultProps = {
+  pokemons: Array(10).fill(""), //Creara un arreglo de 10 posiciones donde cada posición se llena con el valor '' Es decir ['','','','','','','','','','']
+};
+
+export default PokemonList;
