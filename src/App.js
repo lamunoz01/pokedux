89af6af40f0662da9da80551c7538aba499bc995
Laura B. Ahumada M. <lbahumada@open.com.co>
2024-06-12 13:52:21 -0500

    Agregar Loader

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import { Col } from "antd";
 import Searcher from "./components/Searcher";
 import PokemonList from "./components/PokemonList";
 import { getPokemon } from "./api";
-import { getPokemonsWithDetails } from "./actions";
+import { getPokemonsWithDetails, setLoading } from "./actions";
 import { useDispatch, useSelector } from "react-redux";
+import { Spin } from "antd/lib";
 import logo from "./statics/logo.svg";
 import "./App.css";
 
 function App() {
   const pokemons = useSelector((state) => state.pokemons);
+  const loading = useSelector((state) => state.loading);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchPokemons = async () => {
+      dispatch(setLoading(true));
       const pokemonsRes = await getPokemon();
       dispatch(getPokemonsWithDetails(pokemonsRes));
+      dispatch(setLoading(false));
     };
+
     fetchPokemons();
   }, []);
 
@@ -28,7 +33,13 @@ function App() {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons} />
+      {loading ? (
+        <Col offset={12}>
+          <Spin spinning size="large" />
+        </Col>
+      ) : (
+        <PokemonList pokemons={pokemons} />
+      )}
     </div>
   );
 }
--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,16 +1,19 @@
-import { SET_POKEMONS } from "../actions/types"
+import { SET_LOADING, SET_POKEMONS } from "../actions/types";
 
 //Estado Inicial
 const initialState = {
-    pokemons: []
-}
+  pokemons: [],
+  loading: false,
+};
 
 //Reducer - función pura
-export const pokemonsReducer = (state= initialState, action) => {
-    switch(action.type){
-        case SET_POKEMONS:
-            return {...state, pokemons: action.payload};
-        default:
-            return state;
-    }
-}
+export const pokemonsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_POKEMONS:
+      return { ...state, pokemons: action.payload };
+    case SET_LOADING:
+      return { ...state, loading: action.payload };
+    default:
+      return state;
+  }
+};
