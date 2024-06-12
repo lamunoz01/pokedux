82ce9860c0f9f371e0d0163c6d1fdacb5b5effc1
Laura B. Ahumada M. <lbahumada@open.com.co>
2024-06-12 08:19:39 -0500

    Add Redux Hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
-import { connect } from "react-redux";
 import { Col } from "antd";
 import Searcher from "./components/Searcher";
 import PokemonList from "./components/PokemonList";
 import {getPokemon} from './api';
-import { setPokemons as setPokemonsActions } from "./actions";
+import { setPokemons } from "./actions";
+import { useDispatch, useSelector } from "react-redux";
 import logo from "./statics/logo.svg";
 import "./App.css";
 
+function App() {
 
+  const pokemons = useSelector(state => state.pokemons);
+  const dispatch = useDispatch();  
 
-function App({pokemons, setPokemons}) {
-  console.log(pokemons);
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonsRes = await getPokemon();
-      setPokemons(pokemonsRes);
+      dispatch(setPokemons(pokemonsRes));
     };
 
     fetchPokemons();
@@ -34,15 +35,5 @@ function App({pokemons, setPokemons}) {
   );
 }
 
-//Variables necesarias para conectar nuestro componente a redux.
-//Función que recibe un estado y retorna un objeto cuyas propiedades van a ser enviadas a los props del componente que se esta conectando a Redux.
-const mapStateToProps = (state) =>({
-  pokemons: state.pokemons
-});
 
-//Función que recibe el Dispatcher de Redux y retorna un objeto que va a ser mapeado a nuestras propiedades pero con los Actions Creator que se hbaian establecido. 
-const mapDispatchToProps=(dispatch)=>({
-  setPokemons: (value) => dispatch(setPokemonsActions(value))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps) (App);
+export default App;
